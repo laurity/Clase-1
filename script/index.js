0d1c4df6059281357f3228ddbdc5cb59e40c1033
Laura López <lauralopez166@gmail.com>
2023-09-19 13:32:37 +0200

    Tercer commit

--- script/index.js
+++ script/index.js
@@ -1,4 +1,11 @@
-function changeColor(){
-        
-    return 
+const btn=document.querySelector('button');
+const body=document.querySelector('body');
+
+const colors=['blue', 'yellow', 'Green','black', 'white'];
+
+btn.addEventListener('click', fondo);
+
+function fondo(){
+    var indice = parseInt(Math.random()*colors.length);
+    body.style.backgroundColor = colors[indice];
 }
